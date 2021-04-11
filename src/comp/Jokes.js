6202d387ed7e38ac263f2ad22a3a0613d02607e6
stevekutz <stkutz@gmail.com>
2021-04-10 20:27:04 -0500

    added dynamic routing for list of jokes and punchline

--- src/comp/Jokes.js
+++ src/comp/Jokes.js
@@ -1,19 +1,58 @@
 import React from 'react';
+import Joke from './Joke';
+import {Link, Route, useRouteMatch} from 'react-router-dom';
 import {jokes_ten as jokes} from '../data/jokes_data';
+import {
+    JokesContainerDiv,
+    JokesListDiv,
+    JokesPunchlinetDiv,
+
+
+
+} from '../styles/jokes_styles';
+
+
 
 const Jokes = () => {
+
+    const { url } = useRouteMatch();
+
+
     return (
-        <div>
-            <h1> Jokes </h1>
-            {jokes.map((joke, index) => {
-                return (
-                    <p key = {jokes.id}> {joke.setup}</p>
-                )
+        <JokesContainerDiv>
+            <JokesListDiv>
+                <h1> Jokes </h1>
+                {jokes.map((joke, index) => {
+                    return (
+                        <p> 
+                            <Link 
+                                key = {joke.id}
+                                to = {`${url}/${joke.id}`}
+                            
+                            > {joke.setup}</Link>
+                        </p>
+                    )
+                
+                
+                })}
             
             
-            })}
+            </JokesListDiv>
+
+
             
-        </div>
+            <Route path = {`${url}/:jokeid`}>
+                <JokesPunchlinetDiv>
+                    <Joke jokes = {jokes}/>
+                
+                
+                </JokesPunchlinetDiv>
+            </Route>
+            <Route exact path = {url}>
+                <p> Ready for the punchline ? </p>
+            </Route>
+
+        </JokesContainerDiv>
     )
 }
 
---
+++ src/styles/jokes_styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const JokesContainerDiv = styled.div`
+    border: 4px solid purple;
+
+
+    display: flex;
+    
+    width: 90%;
+    margin: 0 auto;
+`;
+
+export const JokesListDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 50%;
+
+`;
+
+export const JokesPunchlinetDiv = styled.div`
+    background: yellow;
+    display: flex;
+    flex-direction: column;
+
+
+`;
