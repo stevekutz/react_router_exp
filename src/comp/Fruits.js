cdb4c06f01928167c6e31fe37ca54d1638aad519
stevekutz <stkutz@gmail.com>
2021-04-19 17:01:25 -0500

    added dynamically rendered images per route name

--- src/comp/Fruits.js
+++ src/comp/Fruits.js
@@ -2,15 +2,32 @@ import React from 'react';
 
 import {Link, Route, useParams, useRouteMatch} from 'react-router-dom';
 
+import apple from '../data/img/apple.jpg';
+import banana from '../data/img/banana.jpg';
+import grape from '../data/img/grape.jpg';
 
 const Fruit = () => {
 
     const {fruitType} = useParams();
 
+    let fruitName = fruitType.toString();
+    console.log('fruitName ' ,fruitName);
+
     return (
         <div>
-            Fruit is : {fruitType}
+            <h1> Fruit is :{fruitType} </h1>
+            <div>
+                <img 
+                    style = {{objectFit: 'contain', width: '100px', height: '100px'}}
+                    src = {`../../${fruitType}.jpg`} // images inside public folder
+                    // src = {`../data/img/${fruitType}.jpg`} // Does not work
+                    // src = {"../data/img/apple.jpg"} // Does not work
+
+                    alt = 'apple'
+                />
+            </div>    
         </div>
+
     
     )
 
@@ -24,6 +41,8 @@ const Fruits = () => {
 
     return (
         <div>
+ 
+
             <Link to = {`${url}/apple`}> Apple </Link>
             <Link to = {`${url}/banana`}> Banana </Link>
             <Link to = {`${url}/grape`} > Grape </Link>
